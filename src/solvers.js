990208b50ad3c9d0db8c5d09cc3d0abee71b260a
Hack Reactor Student <student@hackreactor.com>
2017-05-25 11:20:22 -0700

    finds n-rooks solution board

--- src/solvers.js
+++ src/solvers.js
@@ -16,21 +16,40 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined;
-  var solutionBoard = new Board({n:n});
-  console.log(solutionBoard);
-  
-  var countRooksPlaced = 0;
+  var solution = new Board({n: n});
+  // console.log(solution);
+  // if (n === 1) {
+  //   solution.togglePiece(0, 0);
+  //   return solution;
+  // }
+    
+
   for (var topRowIdx = 0; topRowIdx < n; topRowIdx++) {
+    var countRooksPlaced = 0;
+    
+    solution.togglePiece(0, topRowIdx);
+    countRooksPlaced++;
     for (var rowIdx = 1; rowIdx < n; rowIdx++) {
       for (var colIdx = 0; colIdx < n; colIdx++) {
-      
-
+        solution.togglePiece(rowIdx, colIdx);
+        
+        if (solution.hasAnyRooksConflicts()) {
+          solution.togglePiece(rowIdx, colIdx);
+        } else {
+          countRooksPlaced++;
+          
+          if (countRooksPlaced === n) { console.log('this is console logging the solution ' + solution); }
+          if (countRooksPlaced === n) { return solution.rows(); }
+        }
+        
+        //if (countRooksPlaced === n) { console.log('count = n solution', solution.rows()); }
+        //if (countRooksPlaced === n) { return solution.rows(); }
       }
     }
-  }
-
-  
+  } 
+  //   solution.togglePiece(0, topRowIdx);
+  //   countRooksPlaced--;
+  // }
 
 
   // iterate through each row up to n-1
@@ -48,13 +67,13 @@ window.findNRooksSolution = function(n) {
 
 
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution.rows()));
+  return solution.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+window.countNRooksSolutions = function(n) { //fixme
+  var solutionCount = undefined;
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
